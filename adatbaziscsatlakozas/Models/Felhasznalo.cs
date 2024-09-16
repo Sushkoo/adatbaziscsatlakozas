d6b95725e9c50ee290f92f85dae5117513d31279
Sushkoo <szilagyidominik005@gmail.com>
2024-09-16 08:37:15 +0200

    Add project files.

---
+++ adatbaziscsatlakozas/Models/Felhasznalo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace adatbaziscsatlakozas.Models
+{
+    public class Felhasznalo
+    {
+        public int Id { get; set; }
+
+        public string Nev { get; set; }
+
+        public int SzulDatum { get; set; }
+
+        public string Email { get; set; }
+    }
+}
